1a2a10327b2ceedb4ed17cffd8521887ef9ce86e
Darin Berit R S <darinberitrs@gmail.com>
2024-08-03 20:13:25 +0530

    Updated Navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Homepage/Home';
@@ -13,9 +13,11 @@ function App() {
         <Navbar />
         <div className='content'>
           <Routes>
-            <Route path='/' element={ <Home /> } />
-            <Route path='/create' element={ <CreateBlog /> } />
-            <Route path='/blogs/:id' element={ <BlogDetails /> } />
+            <Route path = '/' element={ < Navigate to="/react-blog-app/home" replace/> }/>
+            <Route path = '/react-blog-app' element={ < Navigate to="/react-blog-app/home" replace/> }/>
+            <Route path='/react-blog-app/home' element={ <Home /> } />
+            <Route path='/react-blog-app/create' element={ <CreateBlog /> } />
+            <Route path='/react-blog-app/blogs/:id' element={ <BlogDetails /> } />
             <Route path='*' element={ <NotFound />} />
           </Routes>
         </div>
